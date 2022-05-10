363d44992f235ef9e3d2f9bad6cdcdaa23cbb504
lydia385 <78177457+lydia385@users.noreply.github.com>
2022-05-10 23:50:17 +0200

    signup 50%

--- app/src/main/java/com/example/fronttttttttttttttttttt/SignUp.java
+++ app/src/main/java/com/example/fronttttttttttttttttttt/SignUp.java
@@ -3,20 +3,42 @@ package com.example.fronttttttttttttttttttt;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.versionedparcelable.NonParcelField;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignUp extends Activity {
     private Button suivant ;
     private ImageButton retourS;
-   protected EditText nomprenom ,mail , tel , mdp , mdpc;
+    protected EditText nomprenom ,mail , tel , mdp , mdpc;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +49,12 @@ public class SignUp extends Activity {
         nomprenom=(EditText)findViewById(R.id.nomprenoms);
         mdp=(EditText)findViewById(R.id.mdps);
         mdpc=(EditText)findViewById(R.id.mdpcs);
-
-
+        mAuth = FirebaseAuth.getInstance();
+        mUser =mAuth.getCurrentUser();
         suivant =(Button) findViewById(R.id.suivant);
         retourS =(ImageButton) findViewById(R.id.retourS);
-
-
+        firebaseDatabase =FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("users");
         suivant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,8 +64,52 @@ public class SignUp extends Activity {
                 String mdp2= mdpc.getText().toString().trim();
                 String nompre= nomprenom.getText().toString().trim();
 
+                Log.d("email",String.valueOf(email));
+                mAuth.createUserWithEmailAndPassword(email,mdp1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+
+                            HashMap<String, String> user = new HashMap<String, String>();
+                            User U =new User();
+                           String j =  Integer.toString(User.nbrU) ;
+
+                            user.put("NomPrenom", nompre);
+                            user.put("telephone", tell);
+                            user.put("Email", email);
+                            user.put("Mot de passe ", mdp1);
 
 
+                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+                            db.collection("User").document(j)
+                                    .set(user)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Log.d("cds", "DocumentSnapshot successfully written!");
+                                            Toast.makeText(getApplicationContext(), "The user has been registered ",
+                                                    Toast.LENGTH_SHORT).show();
+
+
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w("hhh", "Error writing document", e);
+                                        }
+                                    });
+                            Toast.makeText(getApplicationContext(),"La connexion a fctné ",Toast.LENGTH_LONG).show();
+
+                        }else{
+                            Toast.makeText(getApplicationContext(),"La connexion a echoué ",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+
+/*x
                 if(nompre.isEmpty()  ){
                     nomprenom.setError("ce champ est obligatoire");
                     nomprenom.requestFocus();
@@ -78,15 +144,15 @@ public class SignUp extends Activity {
                     return;
                 }
                 if(mdp1.length()<6 ){
-                    mdp.setError("ce champ est obligatoire");
+                    mdp.setError("le mot de passe est trop court");
                     mdp.requestFocus();
                     return;
                 }
                 if(!mdp2.equals(mdp1) ){
-                    mdpc.setError("Le mot de passe ne matche pas");
+                    mdpc.setError("Le mots de passe ne sont pas identiques");
                     mdpc.requestFocus();
                     return;
-                }
+                }*/
 
                 startActivity(new Intent(SignUp.this, SignUp2.class));
             }
--- app/src/main/java/com/example/fronttttttttttttttttttt/User.java
+++ app/src/main/java/com/example/fronttttttttttttttttttt/User.java
@@ -2,13 +2,17 @@ package com.example.fronttttttttttttttttttt;
 
 public class User {
     public String fullname,mail,password,tel;
+    static int nbrU;
     public User(){
+        nbrU++;
     }
+
     public User(String fullname,String  mail
             ,String tel,String password){
         this.fullname=fullname;
         this.password=password;
         this.mail=mail;
         this.tel=tel;
+        nbrU=0;
     }
 }
